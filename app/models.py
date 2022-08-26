1827461cf8820dad2f82f18445f6e9448aef457e
Corqi <kowalikm@student.agh.edu.pl>
2022-08-26 23:35:16 +0200

    main site image scaling

--- app/models.py
+++ app/models.py
@@ -8,4 +8,4 @@ class Message(db.Model):
     content = db.Column(db.String(500), nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False, default=datetime.datetime.utcnow)
     modified_at = db.Column(db.DateTime(), nullable=True)
-    user = db.relationship("User", back_populates="chat_messages", lazy=False)
+    # user = db.relationship("User", back_populates="chat_messages", lazy=False)

ca8bef22fc1f8f8feef470a0af96830766b3cefd
Corqi <kowalikm@student.agh.edu.pl>
2022-09-04 19:20:48 +0200

    rough choose page

--- app/views/choose.py
+++ app/views/choose.py
@@ -1,13 +1,11 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 
-from ..app import db
 
 bp = Blueprint('bp_choose', __name__)
 
+
 @bp.route('/choose')
 @login_required
-def game_get():
-    if current_user.is_free == "false":
-        return redirect(url_for('bp_mission.set_mission', mission_type=1))
-    return render_template('game.html')
+def select_character_get():
+    return render_template('choose.html')
